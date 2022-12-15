ec06fa21d0ee4666d8fc29511105943bbf422ef9
cuad0ng <thanhphuoc01012001@gmail.com>
2022-12-15 21:54:03 +0700

    undo swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import { notFound } from "../middlewares/handleError";
-const swaggerUi = require("swagger-ui-express");
+// const swaggerUi = require("swagger-ui-express");
 
-const swaggerDocs = require("../config/swagger.json");
+// const swaggerDocs = require("../config/swagger.json");
 import userRouter from "./user";
 import videoRouter from "./video";
 import imageRouter from "./image";
 import authRouter from "./auth";
 function route(app) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
   app.use("/api/v1/user", userRouter);
   app.use("/api/v1/video", videoRouter);
   app.use("/api/v1/image", imageRouter);
