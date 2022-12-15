37d7ad4a4dc9a8dc55d072a01ef6225882cfa1eb
cuad0ng <thanhphuoc01012001@gmail.com>
2022-12-15 21:55:53 +0700

    undo swagger

--- build/routes/index.js
+++ build/routes/index.js
@@ -6,10 +6,12 @@ var _video = _interopRequireDefault(require("./video"));
 var _image = _interopRequireDefault(require("./image"));
 var _auth = _interopRequireDefault(require("./auth"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var swaggerUi = require("swagger-ui-express");
-var swaggerDocs = require("../config/swagger.json");
+// const swaggerUi = require("swagger-ui-express");
+
+// const swaggerDocs = require("../config/swagger.json");
+
 function route(app) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
   app.use("/api/v1/user", _user["default"]);
   app.use("/api/v1/video", _video["default"]);
   app.use("/api/v1/image", _image["default"]);
